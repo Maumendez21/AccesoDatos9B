8a145fdb4ba52fadb31faf43d95a9a4f660ff306
Maumendez21 <70171181+Maumendez21@users.noreply.github.com>
2021-06-29 07:31:52 -0500

    Primer comit

---
+++ WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebPruebaAccesoSQL.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" href="css/sweetalert2.min.css" />
+<script src="js/sweetalert2.all.min.js" ></script>
+<script src="js/index.js"></script>
+
+<title></title>
+</head>
+<body>
+    <button onclick="msgbox('jjj', 'sss', 'success')" >aaa</button>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Probar Conexión" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Consulta DataReader" />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" Width="496px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" Height="112px" Width="496px"></asp:ListBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="ConsultaDataSet" />
+&nbsp;
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="ObtenDataSet" />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" Height="159px" Width="499px">
+            </asp:GridView>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using ClassSQL;
+
+namespace WebPruebaAccesoSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        ClassAccesoSQL db = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cn = System.Configuration.ConfigurationManager.ConnectionStrings["BDTIENDA"].ConnectionString;
+            if (IsPostBack == false)
+            {
+                db = new ClassAccesoSQL(cn);
+                Session["db"] = db;
+            }
+            else
+            {
+                db = (ClassAccesoSQL)Session["db"];
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection temp = null;
+            string msg = "";
+            temp = db.AbrirConexion(ref msg);
+
+
+            if (temp!=null)
+            {
+
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "1", "msgbox('Correcto', '" + msg + "', 'success')", true);
+            }
+            else
+            {
+
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "6", "msgbox('Error', `" + msg + "`, 'error')", true);
+            }
+            //TextBox1.Text = msg;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlDataReader caja = null;
+            SqlConnection con = null;
+            string msg = "";
+
+            con = db.AbrirConexion(ref msg);
+            //TextBox1.Text = msg;
+            //Page.ClientScript.RegisterStartupScript(this.GetType(), "4", "msgbox(`Correcto`, `" + msg + "`, `success`)", true);
+            caja = db.ConsultarReader("Select * from EMPLEADO", con, ref msg);
+            if (caja!=null)
+            {
+                // la consulta es correcta
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "2", "msgbox(`Correcto`, `" + msg + "`, `success`)", true);
+                ListBox1.Items.Clear();
+                while (caja.Read())
+                {
+                    ListBox1.Items.Add(caja[0] + "  " + caja["NOMBRE"]);
+                }
+
+            }
+            else
+            {
+                //TextBox1.Text = msg;
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "332", "msgbox(`Incorrecto`, `" + msg + "`, `error`)", true);
+            }
+            con.Close();
+            con.Dispose();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            DataSet contenedor = null;
+            SqlConnection cnab = null;
+            string m = "";
+
+            cnab = db.AbrirConexion(ref m);
+            if (cnab != null)
+            {
+                contenedor = db.ConsultaDS("select * from EMPLEADO", cnab, ref m);
+                cnab.Close();
+                cnab.Dispose();
+                if (contenedor != null)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "set", "msgbox(`Correcto`, `" + m + "`, `success`)", true);
+                    GridView1.DataSource = contenedor.Tables[0];
+                    GridView1.DataBind();
+                    Session["Tabla1"] = contenedor;
+                }
+                else
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "errorSet", "msgbox(`Incorrecto`, `" + m + "`, `error`)", true);
+                }
+            }
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            DataSet t = Session["Tabla1"] as DataSet;
+            ListBox1.Items.Clear();
+            ListBox1.Items.Add("Datos recuperados del dataTable 0");
+            DataRow registro = null;
+            for (int w = t.Tables[0].Rows.Count - 1; w >= 0; w--)
+            {
+                registro = t.Tables[0].Rows[w];
+                ListBox1.Items.Add(registro[1] + " -- " + registro[0]);
+
+            }
+        }
+    }
+}
